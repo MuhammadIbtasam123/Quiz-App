465a77836b28a39915ea3ffa047da9cb36f153b5
Muhammad ibtasam <its.ibtasamqureshi123@gmail.com>
2024-01-16 03:07:57 +0500

    V1- Country-Capital || V2- Flag-Country guessing game

---
+++ V2/index.js
@@ -0,0 +1,74 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+// connection with database
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "********",
+  password: "********",
+  port: 5432,
+});
+
+const app = express();
+const port = 3000;
+
+db.connect();
+
+let totalCorrect = 0;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let quiz = [];
+
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
+
+let currentQuestion = {};
+
+// GET home page
+app.get("/", (req, res) => {
+  totalCorrect = 0;
+  nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  // console.log(answer);
+  // console.log(currentQuestion.name.toLowerCase().trim());
+  if (currentQuestion.name.toLowerCase().trim() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  currentQuestion = randomCountry;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
